3e56a75bcc5a89cf08d55b2267dc32005de7736c
Roman <ioniczodack@gmail.com>
2024-04-03 23:46:33 +0200

    Add draw_snake, draw_ladder

--- sc.py
+++ sc.py
@@ -49,33 +49,57 @@ cell6_surface = pg.Surface((70, 70))
 cell6_surface.fill((0, 0, 0))
 
 cells_positions = {
-    1: (255, 155),
-    2: (330, 155),
-    3: (405, 155),
-    4: (255, 230),
-    5: (330, 230),
-    6: (405, 230)
+    cell1_surface: (255, 155),
+    cell2_surface: (330, 155),
+    cell3_surface: (405, 155),
+    cell4_surface: (255, 230),
+    cell5_surface: (330, 230),
+    cell6_surface: (405, 230)
 
 }
 def move_player_to_cell(cell):
     player_position[0], player_position[1] = cells_positions[cell]
     return player_position
 
+def draw_snake(starting_cell, ending_cell):
+    # Create a rectangle object for cell1_surface
+    starting_rect = starting_cell.get_rect()
+    ending_rect = ending_cell.get_rect()
+
+    # Set the top-left position of cell1_surface on the screen
+    starting_rect.topleft = cells_positions[starting_cell]
+    ending_rect.topleft = cells_positions[ending_cell]
+
+    # Draw a line from the center of cell1_rect to the point (370, 300) with a thickness of 5
+    pg.draw.line(screen, (255, 0, 0), starting_rect.center, ending_rect.center, 5)
+
+def draw_ladder(starting_cell, ending_cell):
+    # Create a rectangle object for cell1_surface
+    starting_rect = starting_cell.get_rect()
+    ending_rect = ending_cell.get_rect()
+
+    # Set the top-left position of cell1_surface on the screen
+    starting_rect.topleft = cells_positions[starting_cell]
+    ending_rect.topleft = cells_positions[ending_cell]
+
+    # Draw a line from the center of cell1_rect to the point (370, 300) with a thickness of 5
+    pg.draw.line(screen, (0, 255, 0), starting_rect.center, ending_rect.center, 5)
+
+
 def handle_key_press(key, player_position):
     if key[pg.K_1]:
-        player_position = move_player_to_cell(1)
+        player_position = move_player_to_cell(cell1_surface)
     elif key[pg.K_2]:
-        player_position = move_player_to_cell(2)
+        player_position = move_player_to_cell(cell2_surface)
     elif key[pg.K_3]:
-        player_position = move_player_to_cell(3)
+        player_position = move_player_to_cell(cell3_surface)
     elif key[pg.K_4]:
-        player_position = move_player_to_cell(4)
+        player_position = move_player_to_cell(cell4_surface)
     elif key[pg.K_5]:
-        player_position = move_player_to_cell(5)
+        player_position = move_player_to_cell(cell5_surface)
     elif key[pg.K_6]:
-        player_position = move_player_to_cell(6)
+        player_position = move_player_to_cell(cell6_surface)
     return player_position
-
 
 running = True
 while running:
@@ -93,6 +117,9 @@ while running:
     
     screen.blit(font_surface, (175, 50))
 
+    draw_snake(cell1_surface, cell4_surface)
+    draw_ladder(cell2_surface, cell6_surface)
+
     # Get the keys that are pressed
     key = pg.key.get_pressed()
     player_position = handle_key_press(key, player_position)
@@ -101,7 +128,7 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    # Draw the board surface with centre position (250, 150) at top left
+
     
 
 
