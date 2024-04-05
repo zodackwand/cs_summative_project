04540acc77e7fec37895178e006d13d80fca520c
Roman <ioniczodack@gmail.com>
2024-04-05 19:15:52 +0200

    Add class Board, class Entity

--- sc.py
+++ sc.py
@@ -14,6 +14,18 @@ SCREEN_HEIGHT = 600
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("Snakes and Ladders")
 
+cells_coordinates = [
+    [255, 155],
+    [330, 155],
+    [405, 155],
+    [255, 230],
+    [330, 230],
+    [405, 230], 
+    [255, 305],
+    [330, 305],
+    [405, 305]
+]
+
 # Create a font object to render the text
 font = pg.font.Font(None, 36)
 font_surface = font.render("Welcome to the Snakes and Ladders", False, "White")
@@ -21,9 +33,34 @@ clock = pg.time.Clock()
 
 # Create a player rect with x, y, width, and height
 
+class Board():
+    def __init__(self, rows, columns, cells_list={}):
+        self.rows = rows
+        self.columns = columns
+        self.cells_list = cells_list
+
+    def create_cells(self, coordinates_array):
+        number_of_cells = self.rows * self.columns
+        for i in range(1, number_of_cells+1):
+            coordinates = coordinates_array[i-1]
+            self.cells_list[i] = Cell(position=coordinates)
+            self.cells_list[i].set_color([0, 0, 0])
+
+    def update_cells(self):
+        for i in range(1, len(self.cells_list)+1):
+            screen.blit(self.cells_list[i].surface, self.cells_list[i].rect)
+
+class Entity():
+    def __init__(self, start_cell=None, end_cell=None):
+        self.start_cell = start_cell
+        self.end_cell = end_cell
+
+    def create(self, start_cell, end_cell):
+        pass
+
 
 class Cell():
-    def __init__(self, surface=[70, 70], rect=None, position=[0, 0]):
+    def __init__(self, surface=[70, 70], position=[0, 0]):
         self.surface = pg.Surface(surface)
         self.rect = self.surface.get_rect()
         self.rect.topleft = position
@@ -43,33 +80,6 @@ player.set_color([200, 50, 50])
 board_surface = pg.Surface((300, 300))
 board_surface.fill((255, 255, 255))
 
-cell1 = Cell(position=[255, 155])
-cell1.set_color([0, 0, 0])
-
-cell2 = Cell(position=[330, 155])
-cell2.set_color([0, 0, 0])
-
-cell3 = Cell(position=[405, 155])
-cell3.set_color([0, 0, 0])
-
-cell4 = Cell(position=[255, 230])
-cell4.set_color([0, 0, 0])
-
-cell5 = Cell(position=[330, 230])
-cell5.set_color([0, 0, 0])
-
-cell6 = Cell(position=[405, 230])
-cell6.set_color([0, 0, 0])
-
-cell7 = Cell(position=[255, 305])
-cell7.set_color([0, 0, 0])
-
-cell8 = Cell(position=[330, 305])
-cell8.set_color([0, 0, 0])
-
-cell9 = Cell(position=[405, 305])
-cell9.set_color([0, 0, 0])
-
 def change_position_to_cell(cell):
     player.rect.topleft = cell.rect.topleft
     player.position = cell.position
@@ -83,37 +93,26 @@ def draw_ladder(start, end):
     # Draw a line from the center of thickness of 5
     pg.draw.line(screen, (0, 255, 0), start.rect.center, end.rect.center, 5)
 
-def update_cells():
-    screen.blit(cell1.surface, cell1.rect)
-    screen.blit(cell2.surface, cell2.rect)
-    screen.blit(cell3.surface, cell3.rect)
-    screen.blit(cell4.surface, cell4.rect)
-    screen.blit(cell5.surface, cell5.rect)
-    screen.blit(cell6.surface, cell6.rect)
-    screen.blit(cell7.surface, cell7.rect)
-    screen.blit(cell8.surface, cell8.rect)
-    screen.blit(cell9.surface, cell9.rect)
-
 
 def handle_key_press(key):
     if key[pg.K_1]:
-        player.position = change_position_to_cell(cell1)
+        player.position = change_position_to_cell(board.cells_list[1])
     elif key[pg.K_2]:
-        player.position = change_position_to_cell(cell2)
+        player.position = change_position_to_cell(board.cells_list[2])
     elif key[pg.K_3]:
-        player.position = change_position_to_cell(cell3)
+        player.position = change_position_to_cell(board.cells_list[3])
     elif key[pg.K_4]:
-        player.position = change_position_to_cell(cell4)
+        player.position = change_position_to_cell(board.cells_list[4])
     elif key[pg.K_5]:
-        player.position = change_position_to_cell(cell5)
+        player.position = change_position_to_cell(board.cells_list[5])
     elif key[pg.K_6]:
-        player.position = change_position_to_cell(cell6)
+        player.position = change_position_to_cell(board.cells_list[6])
     elif key[pg.K_7]:
-        player.position = change_position_to_cell(cell7)
+        player.position = change_position_to_cell(board.cells_list[7])
     elif key[pg.K_8]:
-        player.position = change_position_to_cell(cell8)
+        player.position = change_position_to_cell(board.cells_list[8])
     elif key[pg.K_9]:
-        player.position = change_position_to_cell(cell9)
+        player.position = change_position_to_cell(board.cells_list[9])
     return player.position
 
 running = True
@@ -123,14 +122,16 @@ while running:
     screen.fill((0, 0, 0))
     screen.blit(board_surface, (250, 150))
 
-    update_cells()
+    board = Board(3, 3)
+    board.create_cells(cells_coordinates)
+    board.update_cells()
 
     screen.blit(player.surface, player.rect)
     
     screen.blit(font_surface, (175, 50))
 
-    draw_snake(cell1, cell4)
-    draw_ladder(cell2, cell6)
+    draw_snake(board.cells_list[1], board.cells_list[4])
+    draw_ladder(board.cells_list[2], board.cells_list[6])
 
     # Get the keys that are pressed
     key = pg.key.get_pressed()
